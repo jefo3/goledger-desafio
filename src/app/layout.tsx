19d61f4c05211a8ccf5d8a368431064da6f3d90f
jefo3 <jeferson.goncalves.noronha@gmail.com>
2024-01-28 13:34:11 -0300

    chore: Determining roboto as the font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,21 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import '../styles/globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: 'Music | Goledger',
   description: 'striming music by Goledger',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-      <html lang="pt-BR">
-        <body className={inter.className}>{children}</body>
-      </html>
+    <html lang="pt-BR">
+      <body className={roboto.className}>{children}</body>
+    </html>
   )
 }
