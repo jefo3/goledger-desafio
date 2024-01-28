01d9ac92401cf3186c62d98332018dd7325e7f31
jefo3 <jeferson.goncalves.noronha@gmail.com>
2024-01-28 20:22:35 -0300

    chore: define colors primary and secundary

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      colors: {
+        primary: '#3498db',
+        secundary: '#9b59b6',
+      },
     },
   },
   plugins: [],
